5f9cfb0b8335643248ff1befebbbe2bf9acf26ab
Foundsheep <moondeokjong@gmail.com>
2023-11-24 15:03:26 +0900

    [layers] fixed the padding and nn.ModuleList to work as a model with the right shape

--- layers.py
+++ layers.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from torchsummary import summary
 
 
 class BottleneckResidualBlock(nn.Module):
@@ -8,13 +9,13 @@ class BottleneckResidualBlock(nn.Module):
     def __init__(self, first_channel, last_channel, factor, stride):
         super().__init__()
         self.stride = stride
-        self.conv_1 = nn.Conv2d(in_channels=first_channel, out_channels=int(first_channel*factor), kernel_size=1, stride=2)
+        self.conv_1 = nn.Conv2d(in_channels=first_channel, out_channels=int(first_channel*factor), kernel_size=1, stride=1)
 
         # channel to be used in the block
         c = self.conv_1.out_channels
         self.bn_1 = nn.BatchNorm2d(c)
 
-        self.conv_2_dw = nn.Conv2d(in_channels=c, out_channels=c, kernel_size=3, stride=stride, groups=c, padding='same' if stride == 1 else 'valid')
+        self.conv_2_dw = nn.Conv2d(in_channels=c, out_channels=c, kernel_size=3, stride=stride, groups=c, padding=1)
         self.bn_2 = nn.BatchNorm2d(c)
 
         self.conv_2_pw = nn.Conv2d(in_channels=c, out_channels=last_channel, kernel_size=1)
@@ -36,7 +37,7 @@ class MobileNetV2(nn.Module):
     def __init__(self, class_num):
         super().__init__()
         self.class_num = class_num
-        self.layer_1_conv = nn.Conv2d(in_channels=3, out_channels=32, stride=2, kernel_size=3)
+        self.layer_1_conv = nn.Conv2d(in_channels=3, out_channels=32, stride=2, kernel_size=3, padding=1)
         self.layer_2_bottleneck = BottleneckResidualBlock(first_channel=self.layer_1_conv.out_channels, last_channel=16, factor=1, stride=1)
 
         tmp_lst = []
@@ -45,7 +46,7 @@ class MobileNetV2(nn.Module):
                 tmp_lst.append(BottleneckResidualBlock(first_channel=self.layer_2_bottleneck.last_channel, last_channel=24, factor=6, stride=2))
             else:
                 tmp_lst.append(BottleneckResidualBlock(first_channel=24, last_channel=24, factor=6, stride=1))
-        self.layer_3_bottleneck = nn.ModuleList(tmp_lst)
+        self.layer_3_bottleneck = nn.Sequential(*tmp_lst)
 
         tmp_lst = []
         for i in range(3):
@@ -53,7 +54,7 @@ class MobileNetV2(nn.Module):
                 tmp_lst.append(BottleneckResidualBlock(first_channel=self.layer_3_bottleneck[-1].last_channel, last_channel=32, factor=6, stride=2))
             else:
                 tmp_lst.append(BottleneckResidualBlock(first_channel=32, last_channel=32, factor=6, stride=1))
-        self.layer_4_bottleneck = nn.ModuleList(tmp_lst)
+        self.layer_4_bottleneck = nn.Sequential(*tmp_lst)
 
         tmp_lst = []
         for i in range(4):
@@ -61,7 +62,7 @@ class MobileNetV2(nn.Module):
                 tmp_lst.append(BottleneckResidualBlock(first_channel=self.layer_4_bottleneck[-1].last_channel, last_channel=64, factor=6, stride=2))
             else:
                 tmp_lst.append(BottleneckResidualBlock(first_channel=64, last_channel=64, factor=6, stride=1))
-        self.layer_5_bottleneck = nn.ModuleList(tmp_lst)
+        self.layer_5_bottleneck = nn.Sequential(*tmp_lst)
 
         tmp_lst = []
         for i in range(3):
@@ -69,15 +70,15 @@ class MobileNetV2(nn.Module):
                 tmp_lst.append(BottleneckResidualBlock(first_channel=self.layer_5_bottleneck[-1].last_channel, last_channel=96, factor=6, stride=1))
             else:
                 tmp_lst.append(BottleneckResidualBlock(first_channel=96, last_channel=96, factor=6, stride=1))
-        self.layer_6_bottleneck = nn.ModuleList(tmp_lst)
+        self.layer_6_bottleneck = nn.Sequential(*tmp_lst)
 
         tmp_lst = []
         for i in range(3):
             if i == 0:
-                tmp_lst.append(BottleneckResidualBlock(first_channel=self.layer_5_bottleneck[-1].last_channel, last_channel=160, factor=6, stride=2))
+                tmp_lst.append(BottleneckResidualBlock(first_channel=self.layer_6_bottleneck[-1].last_channel, last_channel=160, factor=6, stride=2))
             else:
                 tmp_lst.append(BottleneckResidualBlock(first_channel=160, last_channel=160, factor=6, stride=1))
-        self.layer_7_bottleneck = nn.ModuleList(tmp_lst)
+        self.layer_7_bottleneck = nn.Sequential(*tmp_lst)
 
         self.layer_8_conv = nn.Conv2d(in_channels=self.layer_7_bottleneck[-1].last_channel, out_channels=1280, stride=1, kernel_size=1, padding='same')
         self.layer_9_avp = nn.AdaptiveAvgPool2d(1)
@@ -110,7 +111,7 @@ def test():
 
 def test_model():
     mobilenetv2 = MobileNetV2(class_num=10)
-    print(mobilenetv2)
+    summary(mobilenetv2, (3, 224, 224))
 
 
 if __name__ == "__main__":
